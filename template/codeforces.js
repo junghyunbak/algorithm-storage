bacd14b53a12e973edd5961c3d5c0f11e831d264
jeong57281 <jeong5728@gmail.com>
2023-02-24 20:08:54 +0900

    chore: baekjoon, codeforces js 입력 템플릿 업데이트

--- template/codeforces.js
+++ template/codeforces.js
@@ -17,10 +17,10 @@ global.SI = () => input(); // string input
 global.NI = () => parseInt(input()); // number input
 global.SAI = (sep = ' ') => input().split(sep); // string array input
 global.NAI = (sep = ' ') => input().split(sep).map(Number); // number array input
-global.A = (n, v = null) => Array(n).fill(v);
-global.AA = (n, m, v = null) => Array.from(Array(n), () => Array(m).fill(v));
-global.AAA = (n, m, h, v = null) =>
-  Array.from(Array(n), () => Array.from(Array(m), () => Array(h).fill(v)));
+global.CA = (n, v = null) => Array(n).fill(v); // create array
+global.CAA = (n, m, v = null) => Array.from(Array(n), () => Array(m).fill(v)); // create two dimension array
+global.CAAA = (n, m, h, v = null) =>
+  Array.from(Array(n), () => Array.from(Array(m), () => Array(h).fill(v))); // create three dimension array
 global.P = console.log;
 
 /*
